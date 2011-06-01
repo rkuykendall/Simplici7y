57c1547d76412251e9725321a5583bcc2eba346e
Robert Kuykendall <robert@rkuykendall.com>
2011-06-01 19:18:05 -0400

    User interface changes. Buttons, header, footer, forms and more.

--- app/helpers/versions_helper.rb
+++ app/helpers/versions_helper.rb
@@ -1,2 +1,13 @@
 module VersionsHelper
+  def download_button(version)    
+    if version.file
+      url = ( link_to 'Download', item_download_path(version.item), :class => 'button down' )
+    elsif version.link
+      url = ( link_to 'Webpage', version.link, :class => 'button next', :target => "_blank" )
+    else
+      url = ""
+    end
+    
+    '<div class="sidenote">' + url + '</div>'
+  end
 end

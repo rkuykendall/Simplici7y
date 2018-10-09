526127ec0aa3f2be1a64f3d0b1063cbc644eff31
Robert Kuykendall <robert@rkuykendall.com>
2018-10-09 12:21:05 -0400

    Revert "Added reCaptcha to S7."
    This reverts commit 6026ef7b1d80ee0bdacc37d6a798b665fe9d9792.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -46,14 +46,12 @@ class UsersController < ApplicationController
     # request forgery protection.
     # uncomment at your own risk
     # reset_session
-    
     @user = User.new(params[:user])
-
-    if @user.errors.empty? && verify_recaptcha(:model => @user, :message => "reCAPTCHA was incorrect, please try again.")
-      @user.save
+    @user.save
+    if @user.errors.empty?
       self.current_user = @user
       redirect_back_or_default('/')
-      flash[:notice] = "Thanks for signing up! Welcome to Simplici7y."
+      flash[:notice] = "Thanks for signing up!"
     else
       render :action => 'new'
     end

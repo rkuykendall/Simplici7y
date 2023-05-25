b97cc26735132d66430d6b9a272eb3a5a493762a
Robert Kuykendall <robert@rkuykendall.com>
2023-05-24 22:34:28 -0500

    Fix Heroku build

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn s7.wsgi
--- s7/settings.py
+++ s7/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 from s7.deployment_config import DEBUG
 

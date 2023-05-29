766c4bcf06cbaef2d448f7e71991762542c9b656
Robert Kuykendall <robert@rkuykendall.com>
2023-05-29 12:02:50 -0500

    Fix Heroku

--- s7/wsgi.py
+++ s7/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "s7.settings")
 
 application = get_wsgi_application()

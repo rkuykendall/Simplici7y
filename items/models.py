f38c5cfebd2f2f2c40c4bbe241c1ef9d93856022
Robert Kuykendall <robert@rkuykendall.com>
2023-06-03 11:28:56 -0500

    Lots of template progress

--- items/models.py
+++ items/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
+from django.utils.safestring import mark_safe
+from django.utils.text import slugify
 
 
 def get_upload_path(instance, filename):
@@ -28,11 +31,28 @@ class Item(TimeStampMixin):
     body = models.TextField()
     tc = models.ForeignKey("self", null=True, blank=True, on_delete=models.PROTECT)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    permalink = models.CharField(max_length=255)
+    permalink = models.SlugField(max_length=255, unique=True)
     tags = models.ManyToManyField(Tag)
+
+    # class Meta:
+    #     ordering = ['version_created_at']
 
     def __str__(self):
         return self.name
+
+    # def save(self, *args, **kwargs):
+    #     if not self.pk:
+    #         self.permalink = slugify(self.name)
+    #     super().save(*args, **kwargs)
+
+    def find_version(self):
+        return Version.objects.filter(item=self).latest('created_at')
+
+    def rand_screenshot(self):
+        return Screenshot.objects.order_by('?').first()
+
+    def get_absolute_url(self):
+        reverse('item_detail', args={'permalink': self.permalink})
 
 
 class Version(TimeStampMixin):
@@ -44,6 +64,16 @@ class Version(TimeStampMixin):
 
     def __str__(self):
         return f"{self.item} {self.name} by {self.item.user}"
+
+    def download_button(self):
+        if self.file:
+            url = '<a href="{}" class="button down">Download</a>'.format(self.item.get_absolute_url())
+        elif self.link:
+            url = '<a href="{}" class="button next" target="_blank">Webpage</a>'.format(self.item.get_absolute_url())
+        else:
+            url = ""
+
+        return mark_safe('<div class="sidenote">{}</div>'.format(url))
 
 
 class Download(TimeStampMixin):
--- s7/settings.py
+++ s7/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "storages",
+    'markdownify',
 ]
 
 MIDDLEWARE = (

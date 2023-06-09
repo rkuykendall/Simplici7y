b3457d9b1e755546ff4f4863f055e404664e47da
Robert Kuykendall <robert@rkuykendall.com>
2023-06-09 15:46:00 -0500

    Fix local dev and pagination

--- s7/middleware.py
+++ s7/middleware.py
@@ -1,16 +1,18 @@
 from django.http import HttpResponsePermanentRedirect
 
-
 class RemoveWwwAndHttpsRedirectMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
+        official_domain = "simplici7y.com"
         host = request.get_host().split(":")[0]
         scheme = request.scheme
-        if host and host.startswith("www.") or scheme != "https":
-            new_host = host[4:] if host.startswith("www.") else host
-            new_scheme = "https" if scheme != "https" else scheme
-            new_url = "{}://{}{}".format(new_scheme, new_host, request.get_full_path())
-            return HttpResponsePermanentRedirect(new_url)
+        print(host)
+        if host.endswith(official_domain):
+            if host and host.startswith("www.") or scheme != "https":
+                new_host = host[4:] if host.startswith("www.") else host
+                new_scheme = "https" if scheme != "https" else scheme
+                new_url = "{}://{}{}".format(new_scheme, new_host, request.get_full_path())
+                return HttpResponsePermanentRedirect(new_url)
         return self.get_response(request)

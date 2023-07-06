eefc4c80507774a0d1a9883f827ca2b1f942e639
Robert Kuykendall <robert@rkuykendall.com>
2023-07-06 09:10:44 -0400

    Revert "Fix secret key path"
    This reverts commit 8d66b5169a671c7f13aa4ab9ca6d7b87aaa1ca9f.

--- s7/settings.py
+++ s7/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-SECRET_KEY = os.environ["I7_DJANGO_SECRET_KEY"]
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 ALLOWED_HOSTS = [
     "simplici7y.herokuapp.com",

281d923bd2d3a96818a5e5452ece3171b1105326
Robert Kuykendall <robert@rkuykendall.com>
2023-07-06 21:58:08 -0400

    Only update db_from_env if exists

--- s7/settings.py
+++ s7/settings.py
@@ -112,7 +112,8 @@ DATABASES = {
 import dj_database_url
 
 db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES["default"].update(db_from_env)
+if db_from_env:
+    DATABASES["default"].update(db_from_env)
 
 
 # Password validation

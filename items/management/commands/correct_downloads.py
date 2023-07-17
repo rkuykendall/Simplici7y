e3bcb678f0a2c760d0a4e4e620d2055fee7bfab6
Robert Kuykendall <robert@rkuykendall.com>
2023-07-16 20:24:10 -0500

    More tipnote and admin improvements

--- items/management/commands/correct_downloads.py
+++ items/management/commands/correct_downloads.py
@@ -1,21 +1,22 @@
 from django.core.management.base import BaseCommand
-from django.db import transaction
+from django.db import transaction, connection
 
-from items.models import Item, Download
+from items.models import Item, Version
 
 class Command(BaseCommand):
     help = 'Corrects the downloads count for each item'
 
-    @transaction.atomic
+    @transaction.atomic  # This will ensure all database operations in this function are done as a single transaction
     def handle(self, *args, **options):
-        for item in Item.objects.all():
-            actual_downloads_count = 0
-            for version in item.versions.all():
-                actual_downloads_count += version.downloads.count()
-            discrepancy = item.downloads_count - actual_downloads_count
-            if discrepancy > 0:
-                default_version = item.versions.order_by("-created_at").first()
-                if default_version:
-                    for _ in range(discrepancy):
-                        Download.objects.create(user=None, version=default_version)
-                    self.stdout.write(f'Corrected download count for item {item}')
+        with connection.cursor() as cursor:
+            for item in Item.objects.all():
+                actual_downloads_count = 0
+                for version in item.versions.all():
+                    actual_downloads_count += version.downloads.count()
+                discrepancy = item.downloads_count - actual_downloads_count
+                if discrepancy > 0:
+                    default_version = item.versions.order_by("-created_at").first()
+                    if default_version:
+                        insert_sql = "INSERT INTO items_downloads (version_id) VALUES (%s)"
+                        cursor.executemany(insert_sql, [(default_version.id,)] * discrepancy)
+                        self.stdout.write(f'Corrected download count for item {item.id}')

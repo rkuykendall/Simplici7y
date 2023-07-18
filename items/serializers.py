f980254b37e889a1e1260987c2b561f3b0414d80
Robert Kuykendall <robert@rkuykendall.com>
2023-07-17 22:08:00 -0500

    Lock down API to read-only

--- items/serializers.py
+++ items/serializers.py
@@ -1,21 +1,27 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from .models import Item, Version, Download, Review, Screenshot, Tag
 
-read_only_fields = ["created_at", "updated_at", "id", "slug", "user"]
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ["id", "username", "first_name", "items_count", "reviews_count"]
+        read_only_fields = "__all__"
 
 
 class ItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = Item
         fields = "__all__"
-        read_only_fields = read_only_fields
+        read_only_fields = "__all__"
 
 
 class VersionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Version
         fields = "__all__"
-        read_only_fields = read_only_fields
+        read_only_fields = "__all__"
 
 
 class DownloadSerializer(serializers.ModelSerializer):
@@ -29,14 +35,14 @@ class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = Review
         fields = "__all__"
-        read_only_fields = read_only_fields
+        read_only_fields = "__all__"
 
 
 class ScreenshotSerializer(serializers.ModelSerializer):
     class Meta:
         model = Screenshot
         fields = "__all__"
-        read_only_fields = read_only_fields
+        read_only_fields = "__all__"
 
 
 class TagSerializer(serializers.ModelSerializer):

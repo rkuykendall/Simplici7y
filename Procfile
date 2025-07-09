4dc33533e792a0b8ae198b9ec51a56498af3db5a
Robert Kuykendall <robert@rkuykendall.com>
2025-07-08 20:59:42 -0500

    Try some upload optimizations

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn s7.wsgi
+web: gunicorn s7.wsgi --config gunicorn.conf.py
--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,4 +1,4 @@
-python3.11 -m venv venv
+python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
 ./scripts/migrate.sh
